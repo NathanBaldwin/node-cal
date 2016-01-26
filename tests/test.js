1a9788778053c8ab92ddc2b923fa92afee15d22a
Nathan Baldwin <nathansbaldwin@gmail.com>
2016-01-26 12:13:17 -0600

    created first test

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,13 @@
 const {expect} = require('chai');
 
+const {execSync} = require('child_process');
+
 describe('test suite', () => {
-  it('true should be true', () => {
-    expect(true).to.be.true;
+  it('hould handle the current month', () => {
+    const goal = execSync('cal').toString();
+    const output = execSync('./cal.js').toString();
+
+    expect(output).to.equal(goal);
   });
 });
 
